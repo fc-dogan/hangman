e6b098d8bf4b16560a75e781b8e7779df9e9d37b
Fatma <fceyda@gmail.com>
2020-09-15 13:42:20 -0700

    fail SetWord test to add a new word to state

--- src/_tests_/reducers/game-word-reducer.test.js
+++ src/_tests_/reducers/game-word-reducer.test.js
@@ -1,7 +1,16 @@
 import gameWordReducer from '../../reducers/game-word-reducer';
-
+ const wordList = ["javascript", "pyton", "csharp"];
+ let action;
 describe('gameWordReducer', () => {
   test('Should return a default state if no action type passed into reducer', () => {
     expect(gameWordReducer(undefined, { type: null })).toEqual("");
-  })
+  });
+  
+    test('Should successfully add a new word to state', () => {
+      action = {
+        type: "SET_WORD",
+        gameWord: wordList[0]
+      }
+      expect(gameWordReducer(undefined, action)).toEqual("javascript")
+    })
 })
