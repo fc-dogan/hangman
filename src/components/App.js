a3353e5fa3ff7fe095a09e1d65b039b3993f4aa6
Fatma <fceyda@gmail.com>
2020-09-15 10:58:39 -0700

    create a diagram and gameControl component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GameControl from './GameControl';
+
+function App() {
+  return (
+    <React.Fragment>
+      <GameControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/GameControl.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class GameControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <h1>Game control</h1>
+     );
+  }
+}
+ 
+export default GameControl;
