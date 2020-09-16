d73cf395dd0f45f0619d2be97b62f0bfd1a69ac0
Fatma <fceyda@gmail.com>
2020-09-16 11:43:50 -0700

    pass the test for reset wrong letter count to 0

--- src/_tests_/reducers/wrong-letter-reducer.test.js
+++ src/_tests_/reducers/wrong-letter-reducer.test.js
@@ -5,7 +5,10 @@ describe('wrongLetterReducer', () => {
   test('Should return default state if no action type is passed to the reducer', () => {
     expect(wrongLetterReducer(undefined, {type: null})).toEqual(0)
   });
-  test('Should increment wrongLetters', () => {
+  test('Should increment wrongLetter', () => {
     expect(wrongLetterReducer(undefined, {type: c.WRONG_LETTER})).toEqual(1)
+  });
+  test('Should reset wrongLetter count to 0', () => {
+    expect(wrongLetterReducer(4, {type: c.RESET_COUNT})).toEqual(0)
   });
 })
