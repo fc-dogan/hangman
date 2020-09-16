76fa9f1e7c0dbc9ca2523c47c0d427acf9c92a6e
Fatma <fceyda@gmail.com>
2020-09-16 11:42:28 -0700

    pass the test for increment wrong letter

--- src/_tests_/reducers/wrong-letter-reducer.test.js
+++ src/_tests_/reducers/wrong-letter-reducer.test.js
@@ -4,5 +4,8 @@ import wrongLetterReducer from '../../reducers/wrong-letter-reducer';
 describe('wrongLetterReducer', () => {
   test('Should return default state if no action type is passed to the reducer', () => {
     expect(wrongLetterReducer(undefined, {type: null})).toEqual(0)
-  })
+  });
+  test('Should increment wrongLetters', () => {
+    expect(wrongLetterReducer(undefined, {type: c.WRONG_LETTER})).toEqual(1)
+  });
 })
