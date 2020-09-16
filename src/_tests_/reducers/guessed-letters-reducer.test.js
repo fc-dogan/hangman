e9bbaca5d47493c70055e1a78ffe7f069ec3300c
Fatma <fceyda@gmail.com>
2020-09-16 11:25:27 -0700

    pass second test for guessedLetter reducer

--- src/_tests_/reducers/guessed-letters-reducer.test.js
+++ src/_tests_/reducers/guessed-letters-reducer.test.js
@@ -5,4 +5,13 @@ describe('guessedLetterReducer', () => {
   test('Should return default state if no action type is passed to the reducer', () => {
     expect(guessedLettersReducer([], {type:null})).toEqual([])
   });
+
+  test("Should successfully add a letter to the gussedLetter array", () => {
+    const currentState = [ "a", "b" ];
+    const action = {
+      type: c.ADD_LETTER,
+      letter: "c"
+    };
+    expect(guessedLettersReducer(currentState, action)).toEqual([  "a", "b", "c" ]);
+  });
 });
