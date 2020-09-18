e673c8b522ed9ec55bc88e6b22714da4b5a4ce9f
Fatma <fceyda@gmail.com>
2020-09-17 23:10:48 -0700

    Updates

---
+++ precache-manifest.331a917e998191285be1cfdfe96d1ae7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6cd499d8c4e916284f30da2dd139e7",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "86e5c7e8e0b3061bf748",
+    "url": "/hangman/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bcdc818f4da1e6a64b1e",
+    "url": "/hangman/static/css/main.d3fecd2a.chunk.css"
+  },
+  {
+    "revision": "86e5c7e8e0b3061bf748",
+    "url": "/hangman/static/js/2.706ac020.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hangman/static/js/2.706ac020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcdc818f4da1e6a64b1e",
+    "url": "/hangman/static/js/main.4aeb37fd.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  },
+  {
+    "revision": "80857def9da77b2fce792cafe6b1099b",
+    "url": "/hangman/static/media/hman4.80857def.png"
+  },
+  {
+    "revision": "6808a1b7040e7b56f3f5590aac32e9a5",
+    "url": "/hangman/static/media/hman5.6808a1b7.png"
+  },
+  {
+    "revision": "678478078023b666ede350ce8a58c1a9",
+    "url": "/hangman/static/media/hman6.67847807.png"
+  },
+  {
+    "revision": "d36bec569a1ce740fac60cd442c60e5e",
+    "url": "/hangman/static/media/hman7.d36bec56.png"
+  }
+]);
